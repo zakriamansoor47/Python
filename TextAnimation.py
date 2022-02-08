1402b38b5d8565d0cde9e014ac17b7af50ca8d84
Zakria Mansoor <56433510+zakriamansoor47@users.noreply.github.com>
2022-02-08 12:10:12 +0500

    Update TextAnimation.py

--- TextAnimation.py
+++ TextAnimation.py
@@ -16,23 +16,20 @@ def textanimation(stringvalue, letters = 50, speed = 0.12, spaces = 20):
     # Created 3 integers
     word = 0
     word2 = 0
-    word3 = 0
+    word3 = len(list1)
     text = "" # empty string
     # Now we will Count how much letters in User Text
-    for dl in list1:
-        word3 +=1
     while(True):
         if word < word3:
             list2.append(list1[word])
             word +=1 
-            for i in list2: # Counting how much letters in Second List
-                word2 +=1
+            word2 = len(list2) #counting the letters in list2
             if word2 >=letters: # check if the letters in Second list is >= 2nd argument of function
                 lol = list2[-letters:] # Removeing frist letter from list
                 time.sleep(speed) # Speed of moving of text
                 for let in lol: # converting list into string and print it
                     text += let
-                    print(f"----------------------------------------|{text}|----------------------------------------", end="\r")
+                    print(f"|{text}|", end="\r")
                 else: # when the letters in list ends we will set the value of text to empty string
                     text =""
             word2 = 0
@@ -43,4 +40,4 @@ def textanimation(stringvalue, letters = 50, speed = 0.12, spaces = 20):
 user = input("Enter Something\n")
 system('cls')
 print("\n\n\n")
-textanimation(user, 50, 0.12, 35)
+textanimation(user)
